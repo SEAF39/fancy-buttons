3c93d97b3283f1591c1e488eadfb3f40b357f0b5
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-22 04:50:25 -0400

    Create project components

---
+++ src/components/CounterButton.jsx
@@ -0,0 +1,12 @@
+/* CounterButton.jsx */
+import React from 'react';
+
+function CounterButton() {
+  return (
+    <button className="CounterButton">
+      You clicked me X amount of times
+    </button>
+  );
+}
+
+export default CounterButton;
---
+++ src/components/TextRepeaterButton.jsx
@@ -0,0 +1,13 @@
+/* TextRepeaterButton.jsx */
+
+import React from 'react';
+
+function TextRepeaterButton() {
+  return (
+    <button className="TextRepeaterButton">
+      <span>I like this text</span>
+    </button>
+  );
+}
+
+export default TextRepeaterButton;
