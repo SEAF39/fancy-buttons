76292b6d768ead8bc8c890299cb98c76551cbd73
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-24 17:07:42 -0400

    use state

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -5,7 +5,7 @@ function AngryButton() {
   const [anger, setAnger] = useState(0);
 
   return (
-    <button style={{ backgroundColor: `rgba(255, 0, 0, ${anger})` }} className="AngryButton">
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
